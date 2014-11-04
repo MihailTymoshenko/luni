35b2c4f3cd223b2fa5b5693d828f4c1cf6bdb27d
Misha <mihail.tymoshenko@gmail.com>
2014-11-04 15:55:38 +0200

    merge conflict resolved

--- javenberg/src/main/java/First.java
+++ javenberg/src/main/java/First.java
@@ -5,7 +5,6 @@
  */
 public class First {
     public static final String TEST="TEST23";
-    public static final String TEST="test";
     public static void main(String[] args) {
         System.out.println();
 
